2fe109cf127428ef01336ef1d8d3141668c74270
John Doe <kimonroxd@gmail.com>
2023-05-20 20:37:55 +0200

    make failure environment

--- environments/failure.py
+++ environments/failure.py
@@ -8,20 +8,38 @@ class Failure(gym.Env):
 
     metadata = {"render_modes": ["human"], "render_fps": 30}
 
-    def __init__(self, arg1, arg2, ...):
+    def __init__(self, n_states: int = 3, time_horizon: int = 10):
         super().__init__()
-        self.action_space = spaces.Discrete(3)
-        self.observation_space = spaces.Discrete(3)
+        self.observation_space = spaces.Discrete(n_states)
+        self.action_space = self.observation_space
+        self.current_state = self.observation_space.sample()
+        self.time_horizon = time_horizon
+        self.time_step = 0
 
     def step(self, action):
+        observation = action
+        reward = 1 if action == 0 and self.current_state == 0 else 0
+        self.time_step += 1
+        terminated = self.time_step == self.time_horizon
+
+        truncated = False
+        info = {"state": self.current_state, "action": action, "reward": reward}
+
+        if terminated:
+            observation, info = self.reset()
+
+        self.current_state = action
         return observation, reward, terminated, truncated, info
 
     def reset(self, seed=None, options=None):
-        ...
+        self.current_state = self.observation_space.sample()
+        observation = self.current_state
+        
+        info = {"reset": True}
         return observation, info
 
     def render(self):
-        ...
+        pass
 
     def close(self):
-        ...
+        pass
