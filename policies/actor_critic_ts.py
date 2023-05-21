cf5f980828671eff094c277f1d15cd3a3f450f58
John Doe <kimonroxd@gmail.com>
2023-05-22 01:48:30 +0200

    fix

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -124,7 +124,7 @@ class ActorCriticTSPolicy(ActorCriticPolicyDepth0):
         
         mean_actions_logits = th.ones((batch_size, self.action_space.n), device=actions.device) / self.action_space.n
         mean_actions_logits[:, 0] += 1
-        mean_actions_logits += values[:, 0] * 0
+        mean_actions_logits += values[0, 0] * 0
 
         distribution = self.action_dist.proba_distribution(action_logits=mean_actions_logits)
         log_prob = distribution.log_prob(actions)
