edad9b5c049e99b40ee8176aedba50c9a19d78ff
John Doe <kimonroxd@gmail.com>
2023-05-21 13:11:02 +0200

    fix

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -130,7 +130,7 @@ class ActorCriticTSPolicy(ActorCriticPolicyDepth0):
                 print("This should not happen! observation not in our dictionary")
                 leaves_observations, rewards, first_action = self.cule_bfs.bfs(obs, self.cule_bfs.max_depth)
                 self.obs2leaves_dict[hash_obs] = leaves_observations, rewards, first_action
-            all_leaves_obs.append(leaves_observations.reshape(-1, 1))
+            all_leaves_obs.append(leaves_observations)
             all_rewards.append(rewards)
             all_first_actions.append(first_action)
             # Preprocess the observation if needed
