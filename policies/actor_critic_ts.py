8cf551143ea8c5d6afe828ce74bbd7a07fd28a38
John Doe <kimonroxd@gmail.com>
2023-05-22 01:14:24 +0200

    fix

--- policies/actor_critic_ts.py
+++ policies/actor_critic_ts.py
@@ -55,7 +55,7 @@ class ActorCriticTSPolicy(ActorCriticPolicyDepth0):
         log_prob = distribution.log_prob(actions)
         
         self.time_step += 1
-        return actions.reshape(-1, 1), th.Tensor([0], device=obs.device).reshape(-1, 1), log_prob
+        return actions.reshape(-1, 1), th.tensor([0], device=obs.device).reshape(-1, 1), log_prob
 
     def evaluate_actions(self, obs: th.Tensor, actions: th.Tensor) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
         """
@@ -75,7 +75,7 @@ class ActorCriticTSPolicy(ActorCriticPolicyDepth0):
 
         distribution = self.action_dist.proba_distribution(action_logits=mean_actions_logits)
         log_prob = distribution.log_prob(actions)
-        return th.Tensor([0], device=obs.device).reshape(-1, 1), log_prob, distribution.entropy()
+        return th.tensor([0], device=obs.device).reshape(-1, 1), log_prob, distribution.entropy()
 
     def hash_obs(self, obs):
         return obs
