e410b1bd638540c356c742818a7cc6895df28479
Kimon Protopapas <kimon.protopapas@gmail.com>
2023-06-15 21:19:53 +0200

    add help to run

--- run.sh
+++ run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 
-while getopts t:w:e:dc flag
+while getopts t:w:e:dch flag
 do
     case "${flag}" in
         t) run_type=${OPTARG};;
         w) weights=${OPTARG};;
         d) debug='-m pdb';;
         c) cumulative='--is_cumulative_mode=true';;
-        e) env_name=${OPTARG}
+        e) env_name=${OPTARG};;
+        h) help=true
     esac
 done
-    
 
-if [[ "$run_type" == "train" ]]
+if [[ $help ]]
+then
+    echo 'run.sh -t run_type -e env_name [-w weights -d debug -c cumulative]'
+    exit 1
+elif [[ "$run_type" == "train" ]]
 then
     echo 'Training'
     python $debug main.py --env_name=$env_name --tree_depth=2 --run_type=train --total_timesteps=300000 $cumulative
