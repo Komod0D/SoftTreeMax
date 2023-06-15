a76693010c6d858f8b71f856dcc64b00af9f9425
Kimon Protopapas <kimon.protopapas@gmail.com>
2023-06-15 21:10:40 +0200

    update run file

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 
-while getopts t:w:dc flag
+while getopts t:w:e:dc flag
 do
     case "${flag}" in
         t) run_type=${OPTARG};;
         w) weights=${OPTARG};;
         d) debug='-m pdb';;
-        c) cumulative='--is_cumulative_mode=true'
+        c) cumulative='--is_cumulative_mode=true';;
+        e) env_name=${OPTARG}
     esac
 done
     
@@ -15,7 +16,7 @@ done
 if [[ "$run_type" == "train" ]]
 then
     echo 'Training'
-    python $debug main.py --env_name=Failure --tree_depth=2 --run_type=train --total_timesteps=300000 $cumulative
+    python $debug main.py --env_name=$env_name --tree_depth=2 --run_type=train --total_timesteps=300000 $cumulative
 else 
     echo 'Testing'
     if [[ "$weights" == "" ]]
@@ -24,6 +25,6 @@ else
     
     else
         echo "Using weights from $weights"
-        python $debug main.py --env_name=Failure --tree_depth=2 --run_type=evaluate --model_filename=$weights
+        python $debug main.py --env_name=$env_name --tree_depth=2 --run_type=evaluate --model_filename=$weights
     fi
 fi
